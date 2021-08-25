767daab0ca68182883b3b6f3cddc4bd8fbebab91
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-25 08:17:55 +0530

    cccccccc

--- eShop.Web/Controllers/AuthenticateController.cs
+++ eShop.Web/Controllers/AuthenticateController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -11,7 +9,7 @@ namespace eShop.Web.Controllers
     public class AuthenticateController : Controller
     {
         [Route("/authenticate")]
-        public async Task<IActionResult> Authenticate([FromQuery]string usr, [FromQuery]string pwd)
+        public async Task<IActionResult> Authenticate([FromQuery] string usr, [FromQuery] string pwd)
         {
             if (usr == "admin" && pwd == "adminadmin")
             {
@@ -26,7 +24,7 @@ namespace eShop.Web.Controllers
                 var userPrincipal = new ClaimsPrincipal(userIdentity);
 
                 await HttpContext.SignInAsync("eShop.CookieAuth", userPrincipal);
-            }            
+            }
 
             return Redirect("/outstandingorders");
         }
--- eShop.Web/Startup.cs
+++ eShop.Web/Startup.cs
@@ -1,18 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using eShop.Web.Data;
 using eShop.UseCases.SearchProductScreen;
 using eShop.UseCases.PluginInterfaces.DataStore;
-using eShop.ShoppingCart.LocalStorage;
 // using eShop.DataStore.HardCoded;
 using eShop.DataStore.SQL.Dapper;
 using eShop.UseCases.ViewProductScreen;
@@ -56,18 +48,18 @@ namespace eShop.Web
             services.AddServerSideBlazor();
 
             services.AddTransient<JsNavigator>();
-            
+
             //services.AddSingleton<IProductRepository, ProductRepository>();
             //services.AddSingleton<IOrderRepository, OrderRepository>();
             services.AddScoped<IShoppingCartStateStore, ShoppingCartStateStore>();
-            
+
             services.AddTransient<IOrderService, OrderService>();
 
             services.AddTransient<IDataAccess>(sp => new DataAccess(Configuration.GetConnectionString("Default")));
             services.AddTransient<IProductRepository, ProductRepository>();
             services.AddTransient<IOrderRepository, OrderRepository>();
 
-            services.AddTransient<IShoppingCart, eShop.ShoppingCart.LocalStorage.ShoppingCart>();            
+            services.AddTransient<IShoppingCart, eShop.ShoppingCart.LocalStorage.ShoppingCart>();
             services.AddTransient<ISearchProductUseCase, SearchProductUseCase>();
             services.AddTransient<IViewProductUseCase, ViewProductUseCase>();
             services.AddTransient<IAddProductToCartUseCase, AddProductToCartUseCase>();
